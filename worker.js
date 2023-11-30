e5931a6e5fdf8bed48b4dd2440ab4ab734d158ae
Petar Georgiev <petiyo07@abv.bg>
2023-11-30 13:15:32 +0200

    app done

---
+++ worker.js
@@ -0,0 +1,17 @@
+import {
+    BlobWriter,
+    HttpReader,
+    TextReader,
+    ZipWriter,
+} from "https://unpkg.com/@zip.js/zip.js/index.js";
+
+
+onmessage = async (e) => {
+    const falseA = e.data.falseAnswers;
+    const trueA = e.data.trueAnswers
+    const text = `${trueA} / ${Number(falseA) + Number(trueA)}`
+    const zipWriter = new ZipWriter(new BlobWriter("application/zip"));
+    await zipWriter.add("QuizScore.txt", new TextReader(text))
+    const blob = await zipWriter.close()
+    postMessage(blob)
+}
